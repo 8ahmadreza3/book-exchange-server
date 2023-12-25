75c6ab092eaf8487c29799ee7cbfc5b8f59cb8a0
8ahmadreza3 <8arhp3@gmail.com>
2023-12-25 17:10:20 +0330

    fix: exception handler

--- app/index.js
+++ app/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 require('./middlewares')(app)
 require('./routes')(app)
+require('./middlewares/exception')(app)
 require('./middlewares/404')(app)
 
 module.exports = (port) => {
