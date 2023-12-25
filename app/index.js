f53f5d64cb426a45108aff8c6ce7b662f65987de
8ahmadreza3 <8arhp3@gmail.com>
2023-12-25 14:34:26 +0330

    basic route app

--- app/index.js
+++ app/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+require('./middlewares')(app)
 module.exports = (port) => {
   app.listen(port, () => {
     console.log(`app is running on port:${port}`)
