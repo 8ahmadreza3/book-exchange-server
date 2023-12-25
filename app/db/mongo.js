a8af48694c01b95e514fccf967995fba8111715d
8ahmadreza3 <8arhp3@gmail.com>
2023-12-25 17:27:41 +0330

    fix: connect to mongodb

---
+++ app/db/mongo.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+const { MONGO_DB, MONGO_HOST, MONGO_PORT } = process.env
+
+mongoose.connection.on('error', error => {
+  console.log('mongodb connection failed', error.message)
+})
+const startMongoDB = () => {
+  mongoose.connect(`mongodb://${MONGO_HOST}:${MONGO_PORT}/${MONGO_DB}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+}
+
+module.exports = startMongoDB
--- app/index.js
+++ app/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+require('./db')
 require('./middlewares')(app)
 require('./routes')(app)
 require('./middlewares/exception')(app)
