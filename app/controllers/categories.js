0df5a19e1b9704c8b0114813e889465ef7a8f376
8ahmadreza3 <8arhp3@gmail.com>
2023-12-26 17:44:14 +0330

    fix: update category

--- app/controllers/categories.js
+++ app/controllers/categories.js
@@ -45,3 +45,28 @@ module.exports.categoryDelete = async (req, res, next) => {
     next(error)
   }
 }
+
+module.exports.patchCategory = async (req, res, next) => {
+  try {
+    const { category } = req.params
+    if (!category) {
+      return res.status(404).send({
+        error: true,
+        message: 'Invalid'
+      })
+    }
+    const { n, nModified } = await CategoriesModel.updateOne({ categoryName: category }, { ...req.body })
+    if (n === 0 || nModified === 0) {
+      return res.status(404).send({
+        error: true,
+        message: 'can not update'
+      })
+    }
+    res.send({
+      success: true,
+      message: `Category ${category}'s information has been updated.`
+    })
+  } catch (error) {
+    next(error)
+  }
+}
--- app/routes/categories.js
+++ app/routes/categories.js
@@ -5,5 +5,6 @@ const categoriesController = require('../controllers/categories')
 router.get('/', categoriesController.categoriesList)
 router.post('/', categoriesController.categoryAdd)
 router.delete('/:category', categoriesController.categoryDelete)
+router.patch('/:category', categoriesController.patchCategory)
 
 module.exports = router
