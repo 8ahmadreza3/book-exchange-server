9cb6406f0411d71efac56527d7c7f1ab2c66ca60
8ahmadreza3 <8arhp3@gmail.com>
2023-12-27 17:37:14 +0330

    fix: user router

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,8 +1,10 @@
 const categoriesRouter = require('./categories')
 const allBooksRouter = require('./allBooks')
 const authorsRouter = require('./authors')
+const usersRouter = require('./users')
 
 module.exports = (app) => {
+  app.use('/user', usersRouter)
   app.use('/categories', categoriesRouter)
   app.use('/books', allBooksRouter)
   app.use('/authors', authorsRouter)
