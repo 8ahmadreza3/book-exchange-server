b6d91c43e8daeaeb58dcc72c20440781a28774ab
8ahmadreza3 <8arhp3@gmail.com>
2023-12-27 17:37:59 +0330

    fix: users model

---
+++ app/models/userModel.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+const usersSchema = new mongoose.Schema({
+  Name: String,
+  userName: {
+    type: String,
+    unique: true
+  },
+  phone: {
+    type: String,
+    unique: true
+  },
+  img: String,
+  state: String,
+  city: String
+})
+
+const userModel = mongoose.model('Category', usersSchema)
+
+module.exports = userModel
