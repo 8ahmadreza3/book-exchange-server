3e9a5d09e671363e078d828d29d52b4e9403ae62
8ahmadreza3 <8arhp3@gmail.com>
2023-12-27 20:05:32 +0330

    fix: authorization

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,10 +2,11 @@ const categoriesRouter = require('./categories')
 const allBooksRouter = require('./allBooks')
 const authorsRouter = require('./authors')
 const usersRouter = require('./users')
+const auth = require('../services/tokenService')
 
 module.exports = (app) => {
   app.use('/user', usersRouter)
   app.use('/categories', categoriesRouter)
   app.use('/books', allBooksRouter)
-  app.use('/authors', authorsRouter)
+  app.use('/authors', [auth], authorsRouter)
 }
