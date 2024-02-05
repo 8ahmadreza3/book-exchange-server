1629f5cc9527b1acfcfdb0bd92bd8c8712ed34d4
8ahmadreza3 <8arhp3@gmail.com>
2024-02-05 17:15:27 +0330

    fix change password with number

---
+++ app/controllers/authSMS/forgotPass.js
@@ -0,0 +1,19 @@
+const smsService = require('../../services/smsService')
+
+module.exports = async (req, res, next) => {
+  try {
+    const { number } = req.params
+    const authCode = Math.floor(Math.random() * 90000 + 10000)
+    smsService(number, authCode)
+    res.send({
+      success: true,
+      message: 'Authentication code sent',
+      message_fa: 'کد احراز هویت ارسال شد',
+      data: {
+        authCode
+      }
+    })
+  } catch (error) {
+    next(error)
+  }
+}
