0786ade1e58723f26c1cf996a36b4a92504ca40d
8ahmadreza3 <8arhp3@gmail.com>
2024-02-06 19:40:29 +0330

    bug auth controller

--- app/controllers/authSMS/confirm.js
+++ app/controllers/authSMS/confirm.js
@@ -1,17 +1,8 @@
 const smsService = require('../../services/smsService')
-const UserModel = require('../../models/usersModel')
 
 module.exports = async (req, res, next) => {
   try {
     const { phone } = req.params
-    const user = UserModel.findOne({ phone })
-    if (!user) {
-      return res.send({
-        success: false,
-        message: 'phone not found',
-        message_fa: 'شماره یافت نشد'
-      })
-    }
     const authCode = Math.floor(Math.random() * 90000 + 10000)
     smsService(phone, authCode)
     res.send({
