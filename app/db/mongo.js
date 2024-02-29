a7a390b758a44dc546058a4d06aed1b0526581fb
8ahmadreza3 <8arhp3@gmail.com>
2024-02-29 13:28:52 +0330

    mongo connection

--- app/db/mongo.js
+++ app/db/mongo.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose')
-const { MONGO_DB, MONGO_HOST, MONGO_PORT } = process.env
+const { MONGO_DB, MONGO_HOST, MONGO_PORT, MONGO_USERNAME, MONGO_PASSWORD } = process.env
 
 mongoose.connection.on('error', error => {
   console.log('mongodb connection failed', error.message)
 })
 const startMongoDB = () => {
-  mongoose.connect(`mongodb://${MONGO_HOST}:${MONGO_PORT}/${MONGO_DB}`)
+  mongoose.connect(`mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOST}:${MONGO_PORT}/${MONGO_DB}?authSource=admin&replicaSet=rs0&directConnection=true`)
     .then(() => {
       console.log('connected to mongodb')
     })
