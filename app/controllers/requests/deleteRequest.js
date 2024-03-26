a5452eda50ee59c4e73f4ab28716bc7774b08129
8ahmadreza3 <8arhp3@gmail.com>
2024-03-26 11:24:51 +0330

    delete requests

--- app/controllers/requests/deleteRequest.js
+++ app/controllers/requests/deleteRequest.js
@@ -5,37 +5,21 @@ const AWS = require('../../services/AWS')
 module.exports = async (req, res, next) => {
   try {
     const { requestId } = req.params
-    const request = await RequestsModel.findOne({ _id: requestId })
-    const user = await UsersModel.findOne({ userName })
-    if ((request.owner === userName && !request.getter) || user.isAdmin) {
-      const request = await RequestsModel.findByIdAndDelete(requestId)
-      if (request.awsKey.length > 0) {
-        const remove = AWS.remove(request.awsKey)
-        if (!remove.success) {
-          return res.send(remove)
-        }
+    const { deletedCount } = await RequestsModel.updateOne({ _id: requestId }, req.body)
+    if (deletedCount === 0) {
+      const { deletedCount } = await ApplicantsModel.updateOne({ _id: requestId }, req.body)
+      if (deletedCount === 0) {
+        return res.status(404).send({
+          success: false,
+          message: 'can not delete request',
+          message_fa: 'نمی توان درخواست را حذف کرد'
+        })
       }
-      return res.send({
-        success: true,
-        message: 'Request was removed by the owner of the book',
-        message_fa: 'درخواست توسط صاحب کتاب حذف شد'
-      })
     }
-    if (request.applicants.filter(ap => ap.userName === userName).length > 0 || user.isAdmin) {
-      const newApplicants = request.applicants.filter((applicant) => {
-        return applicant !== userName
-      })
-      await RequestsModel.updateOne({ _id: requestId }, { newApplicants })
-      return res.send({
-        success: true,
-        message: 'Request was removed',
-        message_fa: 'درخواست حذف شد'
-      })
-    }
-    return res.send({
-      success: false,
-      message: 'You can not the delete this request',
-      message_fa: 'شما نمی توانید این درخواست را حذف کنید'
+    res.send({
+      success: true,
+      message: 'request deleted',
+      message_fa: 'درخواست حذف شد'
     })
   } catch (error) {
     next(error)
