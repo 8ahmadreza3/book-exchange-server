943b2e2b70680085f748d97f46a31a192759e3ad
8ahmadreza3 <8arhp3@gmail.com>
2024-03-26 11:24:13 +0330

    book applicants

--- app/controllers/books/infoBook.js
+++ app/controllers/books/infoBook.js
@@ -1,5 +1,6 @@
 const BooksModel = require('../../models/booksModel')
 const RequestsModel = require('../../models/requestsModel')
+const ApplicantsModel = require('../../models/applicantsModel')
 
 module.exports = async (req, res, next) => {
   try {
@@ -20,13 +21,18 @@ module.exports = async (req, res, next) => {
       })
     }
     const requests = await RequestsModel.find({ book: book.name })
+    const presentedRequests = requests.map(async request => {
+      request.createdAt = dateService.toPersianDate(request.createdAt)
+      request.applicants = await ApplicantsModel.find({ requestId: request._id })
+      return request
+    })
     res.send({
       success: true,
       message: 'book information found',
       message_fa: 'اطلاعات کتاب پیدا شد',
       data: {
         book,
-        requests
+        requests: presentedRequests
       }
     })
   } catch (error) {
