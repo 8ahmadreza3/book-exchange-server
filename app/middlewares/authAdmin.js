9a24ecd58a428a126b99157ab4a85ff3440be3f7
8ahmadreza3 <8arhp3@gmail.com>
2024-04-22 12:40:56 +0330

    debug authorization

--- app/middlewares/authAdmin.js
+++ app/middlewares/authAdmin.js
@@ -2,14 +2,14 @@ const tokenService = require('../services/tokenService')
 const UsersModel = require('../models/usersModel')
 
 module.exports = async (req, res, next) => {
-  if (!('authorization' in req.header)) {
+  if (!req.headers.authorization) {
     return res.status(401).send({
       success: false,
       message: 'you are not authorized',
       message_fa: 'شما مجاز به ارسال درخواست نیستید'
     })
   }
-  const token = tokenService.verify(req.header.authorization)
+  const token = tokenService.verify(req.headers.authorization)
   if (!token) {
     return res.status(401).send({
       success: false,
--- app/middlewares/authUser.js
+++ app/middlewares/authUser.js
@@ -1,14 +1,14 @@
 const tokenService = require('../services/tokenService')
 
 module.exports = (req, res, next) => {
-  if (!('authorization' in req.header)) {
+  if (!('authorization' in req.headers)) {
     return res.status(401).send({
       success: false,
       message: 'you are not authorized',
       message_fa: 'شما مجاز نیستید'
     })
   }
-  const token = tokenService.verify(req.header.authorization)
+  const token = tokenService.verify(req.headers.authorization)
   if (!token) {
     return res.status(401).send({
       success: false,
