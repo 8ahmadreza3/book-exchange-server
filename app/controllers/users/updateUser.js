b0542fdfacc5c4c2dc1390b86f6320ea84d4fadf
8ahmadreza3 <8arhp3@gmail.com>
2024-05-03 20:24:48 +0330

    user update

--- app/controllers/users/updateUser.js
+++ app/controllers/users/updateUser.js
@@ -2,15 +2,23 @@ const UsersModel = require('../../models/usersModel')
 
 module.exports = async (req, res, next) => {
   try {
+    const { user } = req.data
     const { userName } = req.params
+    if (user.userName !== userName && !user.isAdmin) {
+      res.send({
+        success: false,
+        message: 'you are not authorized',
+        message_fa: 'شما مجاز نیستید'
+      })
+    }
     const newUserName = req.body.userName.replaceAll(' ', '_')
     if (newUserName && newUserName !== userName) {
       const sameUserName = await UsersModel.findOne({ userName: newUserName })
       if (sameUserName) {
         res.send({
           success: false,
-          message: 'This address is duplicate',
-          message_fa: 'این آدرس تکراری است'
+          message: 'This username is duplicate',
+          message_fa: 'نام کاربری تکراری است'
         })
       }
     }
@@ -21,6 +29,7 @@ module.exports = async (req, res, next) => {
         message_fa: 'کاربر نامعتبر'
       })
     }
+    req.body.isAdmin = false
     const { n, nModified } = await UsersModel.updateOne({ userName }, { ...req.body })
     if (n === 0 || nModified === 0) {
       return res.status(404).send({
